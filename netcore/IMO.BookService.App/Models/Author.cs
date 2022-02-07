8a5af38725c43dfb920340e7f3c327d16145a4ef
Mike Wilkinson <mwilkinson@imohealth.com>
2022-02-07 14:32:47 -0600

    Initial implementation of netcore interview project.

---
+++ netcore/IMO.BookService.App/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace IMO.BookService.App.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+
+        public List<string> Books { get; set; }
+
+        public List<string> Genres { get; set; }
+
+        public Author()
+        {
+            Books = new List<string>();
+            Genres = new List<string>();
+        }
+    }
+}
---
+++ netcore/IMO.BookService.App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using IMO.BookService.App.Models;
+
+
+namespace IMO.BookService.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookAuthors = GetBookAuthors();
+            Console.WriteLine($"Read data. Found {bookAuthors.Items.Length} entries");
+            Console.WriteLine("Aggregating author info.");
+            var authors = GetAuthors(bookAuthors);
+            var output = JsonConvert.SerializeObject(authors, Formatting.Indented);
+            Console.WriteLine("Aggregation done:" + Environment.NewLine + Environment.NewLine);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        static BookAuthors GetBookAuthors()
+        {
+            var text = File.ReadAllText("Samples\\data.json");
+            var bookAuthors = JsonConvert.DeserializeObject<BookAuthors>(text);
+            return bookAuthors;
+        }
+
+        static List<Author> GetAuthors(BookAuthors bookAuthors)
+        {
+            var authors = new List<Author>();
+            var uniqueGroups = bookAuthors.Items.GroupBy(x => x.Author);
+            foreach (var grouping in uniqueGroups)
+            {
+                var author = new Author();
+                author.Name = grouping.Key;
+                foreach (var authorBook in grouping)
+                {
+                    author.Books.Add(authorBook.Title);
+                    author.Genres.AddRange(authorBook.Genres);
+                }
+                author.Genres = author.Genres.Distinct().ToList();
+                authors.Add(author);
+            }
+
+            return authors;
+        }
+    }
+}
